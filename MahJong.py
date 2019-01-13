0f1344ef3728245ced7a3a6300798a2f8ea3f7ac
Peter <46075072+macropeter@users.noreply.github.com>
2019-01-13 17:48:43 +0100

    __Gesamt, Fehler (spieler0 statt self) beseitigt

--- MahJong.py
+++ MahJong.py
@@ -13,20 +13,20 @@ class spieler:
     Kvn=0
     Kve=0
     TP=0
-    Gesamt=0
-    Saldo=0
     
     def __init__(self,name):
         self.name=name
+        self.__Gesamt=0
+        self.__Saldo=0
         
     def BerechneSumme(self):
         global punktestand
-        hilfliste=[punktewertung[k]*getattr(spieler0,k) for k in punktewertung]
+        hilfliste=[werteliste[k]*getattr(self,k) for k in werteliste]
         self.Gesamt=functools.reduce(lambda x,y: x+y,hilfliste)
-        return self.Gesamt
+        return self.__Gesamt
 
     def __str__(self):
-        return self.name+':'+str(self.Gesamt)
+        return self.name+':'+str(self.__Gesamt)
         
 
 spielerliste=[spieler('Spieler0'),
@@ -35,15 +35,13 @@ spielerliste=[spieler('Spieler0'),
               spieler('Spieler3')]
 
 def saldoBerechnung(werte,punkte,neu):
-    
     zn=[0,0,0,0]
     ins=neu[0]+neu[1]+neu[2]+neu[3]
     zn[0]=ins-4*neu[0]
     zn[1]=ins-4*neu[1]
     zn[2]=ins-4*neu[2]
     zn[3]=ins-4*neu[3]
-    
-    
+       
     for i in range(4):
         punkte[i]=punkte[i]+zn[i]
     
@@ -51,7 +49,7 @@ def saldoBerechnung(werte,punkte,neu):
 
 def NeuerSpielstand():
     global spielstand
-    spielstand=[sp.Gesamt for sp in spielerliste]
-    return spielstand
+    spielstand=[sp.__Gesamt for sp in spielerliste]
+    return True
 
 dx=[8,4,14,26]
